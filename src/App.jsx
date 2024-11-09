a30add740cf782f94d76836f5b7c3444dde0e38b
Matthew McNee <matt@acyleris.com>
2024-11-09 13:05:15 +0000

    moved test components to dedicated folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.scss';
-
-import OpenAIPage from './OpenAIPage';
-import EmbeddingTester from './EmbeddingTester';
-import AnswersInput from './AnswersInput';
-import SpeechToText from './SpeechToText';
-import AudioStreamer from './AudioStreamer';
-import AudioTranscriber from './AudioTranscriber';
-import MicToText from './MicToText';
 import AudioCallPage from './call/AudioCallPage';
-
-import Speaker from './call/meeting/Speaker';
-import TextProcessor from './TextProcesser';
-import LoadingPage from './components/LoadingPage';
-import FunctionsTest from './FunctionsTest';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Route for OpenAIPage */}
-        <Route path="/" element={<OpenAIPage />} />
-        
-        {/* Route for EmbeddingTester */}
-        <Route path="/embedding" element={<EmbeddingTester />} />
-        <Route path="/answers" element={<AnswersInput />} />
-        <Route path="/audio" element={<SpeechToText />} />
-        <Route path="/stream" element={<AudioTranscriber />} />
-        <Route path="/mic" element={<MicToText />} />
-        <Route path="/call" element={<AudioCallPage />} />
-
-        {/* <Route path="/speech" element={<Speaker />} /> */}
-
-        <Route path="/text" element={<TextProcessor />} />
-
-        <Route path="/test" element={<FunctionsTest />} />
-
-        <Route path='/loading' element={<LoadingPage/>} />
-
+        <Route path="*" element={<AudioCallPage />} />
       </Routes>
     </Router>
   );
